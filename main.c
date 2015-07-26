0002b323b3733da2a396accc1362f2d0651dabd5
brandonkehl <kehl.brandon@gmail.com>
2015-07-25 23:27:34 -0500

    Added all code

---
+++ main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Time
+//
+//  Created by brandon kehl on 7/24/15.
+//  Copyright (c) 2015 bignerdranch. All rights reserved.
+//
+
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, const char * argv[]) {
+    long secondsSince1970 = time(NULL) + 4000000;
+    printf("It has bemm %ld seconds 1970\n", secondsSince1970);
+    
+    
+    struct tm now;
+    localtime_r(&secondsSince1970,&now);
+    printf("After 4 Million seconds the time and date would be %d-%d-%d %d:%d:%d \n", now.tm_mon,now.tm_mday,now.tm_year + 1900,now.tm_hour,now.tm_min,now.tm_sec);
+
+
+}
